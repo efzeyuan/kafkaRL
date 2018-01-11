0c94f0cbc38bf3b0e3893cd2a546b244d6e765ce
TastyLu <l295439753@163.com>
2018-01-11 14:50:53 +0800

    single agent

---
+++ multi_agent/Env.py
@@ -0,0 +1,19 @@
+from pykafka import KafkaClient
+import gym
+
+env = gym.make('CartPole-v0')
+
+client = KafkaClient("162.105.85.212:9092")
+
+topic_input = client.topics['cartpole-env-output']
+producer = topic_input.get_producer()
+producer.start()
+producer.produce(str(env.reset()))
+
+topic_output = client.topics['cartpole-env-input']
+consumer = topic_output.get_simple_consumer()
+for msg in consumer:
+    if msg is not None:
+        action = int(msg.value)
+	next_state, reward, done, _ = env.step(action)
+	producer.produce(str(next_state)+'|'+str(reward)+'|'+str(done))
